ab3f564b0dea7861932dc49e34529a614c68117e
Konrad <55025128+Konraddd123@users.noreply.github.com>
2023-11-25 00:25:21 +0100

    testy klikania rzeczy

--- Projekt Cywilizacja 2077/Scenes/CameraNodeMovement.gd
+++ Projekt Cywilizacja 2077/Scenes/CameraNodeMovement.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 @export var swipe_speed : float = 0.02
+var mouse=Vector2()
 
 func _input(event):
 	if event is InputEventScreenDrag:
@@ -22,3 +23,44 @@ func dragging_movement(event: InputEventScreenDrag):
 		vecTest[2] = -35
 	
 	global_position = vecTest
+
+
+
+#
+#func _process(delta):
+#	if Input.is_action_just_pressed("mouse_left"):
+#		var mouse_pos = get_viewport().get_mouse_position()
+#		var viewport_size = get_viewport().get_visible_rect().size
+#
+#		# Pozycja myszy w odniesieniu do obszaru widoku
+#		var normalized_mouse_pos = Vector2(
+#			mouse_pos.x / viewport_size.x * 2 - 1,
+#			mouse_pos.y / viewport_size.y * 2 - 1
+#		)
+#
+#		# Uzyskaj pozycję kamery w świecie gry
+#		var from = global_transform.origin
+#
+#		# Uzyskaj kierunek raycastu od kamery w kierunku pozycji myszy
+#		var to = from + global_transform.basis.z * 1000  # Mnożnik (1000) to długość raycastu
+#
+#		var space_state = get_world_3d().direct_space_state
+#		var query = PhysicsRayQueryParameters3D.create(RayOrigin, RayEnd);
+#		var Intersection = SpaceState.intersect_ray(query)
+#		#var result = space_state.intersect_ray(P(from, to))
+#
+#		# Sprawdź, czy raycast trafił w coś
+#		if result.size() > 0:
+#			var collision_point = result.position
+#			var collision_normal = result.normal
+#			# Operacje na punkcie kolizji i normalnej
+#			print("Collision at: ", collision_point)
+#			print("Collision normal: ", collision_normal)
+
+#onready var camera=$Camera3D
+#func get_selection():
+#	var worldspace=get_world_3d().direct_space_state
+#	var start = project_ray_origin(mouse)
+#	var end = project_position(mouse, 1000)
+#	var result = worldspace.intersect_ray(PhysicsRayQueryParameters3D.create(start, end))
+	
